5466afde2960bdb91d79f276a51344df8d71cd3b
Herry <151930033+Mystogaan@users.noreply.github.com>
2024-10-14 21:49:32 +0800

    Add files via upload

---
+++ Posttest6/home.php
@@ -0,0 +1,221 @@
+<?php
+    require 'koneksi.php';
+
+    if(isset($_GET['submit'])) {
+        $comment = $_GET['comment'];
+
+        $sql = "INSERT INTO komen VALUES ('','$comment')";
+        $query = mysqli_query($conn, $sql);
+
+        if($query) {
+            echo "<script>alert('Berhasil menyimpan perubahan...');</script>";
+        } else {
+            echo "<script>alert('Gagal menyimpan perubahan...');</script>";
+        }
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEBSITE PENJUALAN NOVEL</title>
+    <link rel="stylesheet" href="style/home.css">
+</head>
+<body>
+    <!-- Navbar Start -->
+    <?php include 'navbar.php'; ?>
+    <!-- Navbar End -->
+
+    <!-- Main Content Start-->
+    <div class="header-text">
+        <h3>Rekomendasi Herrymedia Untukmu</h3>
+    </div>
+    <div class="container-main">
+        <!-- card novel bumi -->
+        <div class="card-container">
+            <div class="card active" id="card1">
+                <img src="assets/bumi.jpg" alt="Bumi">
+                <div class="info">
+                    <p>Tere Liye</p>
+                    <p>Bumi</p>
+                    <p>Rp 98.100</p>
+                </div>
+            </div>
+            <!-- Pop-up Box for Bumi -->
+            <div class="modal" id="modal1">
+                <div class="modal-content">
+                    <span class="close" data-close="modal1">&times;</span>
+                    <h2>Bumi</h2>
+                    <p>“Namaku Raib, usiaku 15 tahun, kelas sepuluh. Aku anak perempuan seperti kalian, adik-adik kalian, tetangga kalian. Aku punya dua kucing, namanya si Putih dan si Hitam. Mama dan papaku menyenangkan. Guru-guru di sekolahku seru. Teman-temanku baik dan kompak.”
+    
+                        Sinopsis
+    
+                        Tere Liye kembali mengkreasikan imajinasinya kedalam kedalam beberapa rangkaian novel. Bumi, merupakan rangkaian awal dari kisah sekelompok anak remaja berkemampuan istimewa. Menceritakan tentang Raib, Ali, dan Seli yang bertualang ke dunia paralel. Mereka yang istimewa, mampu pergi ke dunia pararel bumi, bertemu dengan klan lain dan berhadapan dengan Tamus yang memiliki ambisi membebaskan si Tanpa Mahkota dan kemudian, menguasai bumi. Perkenalkan, Raib, seorang gadis belia berusia lima belas tahun yang tidak biasa. Dia bisa menghilang. Jangan beritahu siapapun, Itu adalah rahasia terbesar yang tak pernah ia ceritakan pada siapapun, termasuk kedua orangtuanya. Kehidupannya tetap berjalan normal, meskipun untuk dirinya sendiri. Tidak jarang Raib menjahili orang tuanya dengan tiba-tiba menghilang, lalu muncul kembali secara tiba-tiba membuat kaget kedua orangtuanya. Tidak hanya menyuguhkan cerita fantasi, novel ini juga memberikan pesan moral tentang keluarga, dan persahabatan. Tere Liye sukses membangun kisah persahabatan antara Raib, Ali, dan Seli terasa nyata. Hubungan antara Raib dan keluarganya membuat pembaca penasaran sekaligus tersadar akan cara berkomunikasi dengan orang tua. Tere Liye memberikan banyak kejutan di tiap halaman yang direpresentasikan oleh Raib, membuat pembaca dapat menikmati cerita yang seolah tidak akan ada habisnya. Tere Liye berhasil meracik buku ini sebagai bahan baca para pecinta novel sastra maupun fantasi.
+                    </p>
+                </div>
+            </div>
+            <!-- card novel bulan -->
+            <div class="card active" id="card2">
+                <img src="assets/bulan.jpg" alt="Bulan">
+                <div class="info">
+                    <p>Tere Liye</p>
+                    <p>Bulan</p>
+                    <p>Rp 100.000</p>
+                </div>
+            </div>
+            <!-- pop up novel bulan -->
+            <div class="modal" id="modal2">
+                <div class="modal-content">
+                    <span class="close" data-close="modal2">&times;</span>
+                    <h2>Bulan</h2>
+                    <p>Petualangan Raib, Seli, dan Ali berlanjut. Beberapa bulan setelah peristiwa klan bulan, Miss Selena akhirnya muncul di sekolah. Ia membawa kabar menggembirakan untuk anak-anak yang berjiwa petualang seperti Raib, Seli, dan Ali. Miss Selena bersama dengan Av akan mengajak mereka untuk mengunjungi klan matahari selama dua minggu. Av berencana akan bertemu dengan ketua konsil klan matahari, yang menguasai klan matahari sepenuhnya untuk mencari sekutu dalam menghadapi Tamus yang diperkirakan akan bebas dan juga membebaskan raja tanpa mahkota. Sesampainya mereka di Klan matahari, mereka disambut oleh festival bunga matahari. Hal yang tidak pernah disangka oleh Av dan Miss Selena adalah ketua konsil klan matahari yang meminta Raib, Seli, Ali, dan Ily untuk menjadi peserta ke-10 dari festival bunga matahari. Setelah perdebatan yang amat panjang, akhirnya rombongan Raib menerima tawaran itu. Dengan kekuatan yang dimiliki Seli, Raib, dan Ily ditambah dengan senjata berupa panah dan pemukul, mereka bertekad melewati rintangan yang sangat membahayakan keselamatannya itu. Di akhir kisah, Ily telah pergi selama-lamanya meninggalkan keluarganya di Klan bulan juga teman-teman seperjuangannya selama sembilan hari menemukan bunga matahari. Dan Fala, ketua klan matahari telah masuk ke portal ke penjara bayangan bawah tanah. Setelah Serial pertamanya, Bumi, sukses melejit, kini Tere Liye menghadirkan kisah lanjutannya dengan Bulan. Kini anak istimewa itu bernama Seli, masih 15 tahun. Sama halnya seperti remaja yang lain, Seli mendengarkan lagu-lagu yang sedang hits, pergi ke gerai fast food, menonton serial drama dan film. Perbedaannya, Seli bisa mengeluarkan petir. Dan dengan kekuatan itu, Seli bertualang menuju tempat-tempat yang menakjubkan bersama Raib. Dengan bekal hewan tunggangan empat ekor harimau salju sebagai kendaraan mereka selama festival, mereka menghadapi medan berbahaya dan binatang buas dalam hutan yang siap menyerang mereka. Namun, mengapa harus mereka yang mengikuti festival berbahaya ini?
+                    </p>
+                </div>
+            </div>
+            <!-- card novel kudasai -->
+            <div class="card active" id="card3">
+                <img src="assets/kudasai.jpg" alt="Kudasai">
+                <div class="info">
+                    <p>Brian Khrisna</p>
+                    <p>Kudasai</p>
+                    <p>Rp 105.000</p>
+                </div>
+            </div>
+            <!-- pop up novel kudasai -->
+            <div class="modal" id="modal3">
+                <div class="modal-content">
+                    <span class="close" data-close="modal3">&times;</span>
+                    <h2>Kudasai</h2>
+                    <p>Novel Kudasai
+    
+                        Kudasai merupakan novel yang menceritakan tentang kehidupan sepasang suami istri, Chaka dan Twindy, yang menikah karena terpaksa. Di dalam pernikahan mereka, Chaka berperan sebagai bapak rumah tangga yang mengurus dan membersihkan rumah serta mengelola kafe sementara Twindy bekerja sebagai pemimpin di sebuah firma arsitek terkemuka. Kehidupan pernikahan mereka berjalan dengan lancar sampai suatu hari Chaka bertemu mantan kekasihnya dengan kisah mereka berdua yang belum usai. Akankah Chaka tetap mempertahankan rumah tangganya dengan Twindy ataukah ia lebih memilih mantan kekasihnya itu?
+    
+                        Sinopsis Buku
+    
+                        Akibat tindakan bodohnya, Chaka terpaksa menikahi Twindy, seorang alpha female luar biasa yang memimpin sebuah firma arsitek terkemuka. Chaka yang seumur hidupnya hanya memiliki dua keahlian, yaitu bernapas dan memasak; mau tidak mau harus menjalani kehidupan pernikahan layaknya sedang menjalani tutorial siksa kubur. Selama dua tahun pernikahan, Chaka tidak pernah sekali pun berani melawan Twindy yang galak banget kayak istri mudanya Firaun. Meskipun begitu, Chaka selalu menyayangi Twindy yang menjadi tulang punggung utama di rumah. Chaka sendiri lebih banyak mengurusi pekerjaan sehari-hari di rumah, seperti memasak, mencuci piring, membersihkan WC, menyikat tangki air, dan mengelola kafe, yang mayoritas sahamnya dimiliki oleh Twindy. Semua barang di rumah dan kafe juga adalah milik Twindy, sedangkan barang yang Chaka beli dengan uangnya sendiri hanya sikat gigi dan remote TV. Seolah-olah kehidupan penuh tangis dan tawa itu belum cukup, Chaka tidak sengaja bertemu dengan mantan pacar yang dulu ditinggalkannya untuk menikah dengan Twindy. Segala hal yang belum selesai di antara mereka pun membawa Chaka ke pusaran yang meskipun ia sekuat tenaga berenang menjauh, tetapi ia justru semakin terseret mendekat. Apakah Chaka harus membiarkan dirinya terseret dan tenggelam bersama masa lalunya, atau meraih uluran tangan Twindy yang ternyata sedang mengandung anaknya?
+                    </p>
+                </div>
+            </div>
+            <!-- card novel laut -->
+            <div class="card active" id="card4">
+                <img src="assets/lautbercerita.png" alt="Laut Bercerita">
+                <div class="info">
+                    <p>Leila S. Chudori</p>
+                    <p>Laut Bercerita</p>
+                    <p>Rp 100.000</p>
+                </div>
+            </div>
+            <!-- pop up novel laut -->
+            <div class="modal" id="modal4">
+                <div class="modal-content">
+                    <span class="close" data-close="modal4">&times;</span>
+                    <h2>Laut Bercerita</h2>
+                    <p>Buku ini terdiri atas dua bagian, yaitu kisah-kisah tentang kehidupan yang menyoroti aspek sosial, politik, serta keluarga. Penulis berusaha menggambarkan berbagai sisi kehidupan dan kegalauan yang dihadapi setiap karakter dalam novel ini, dengan latar belakang suasana pantai yang menggugah semangat hidup. Dalam novel ini, pembaca diajak merenungkan makna kehidupan serta nilai-nilai kemanusiaan yang sangat berharga.
+                    </p>
+                </div>
+            </div>
+            <!-- card novel matahari -->
+            <div class="card active" id="card5">
+                <img src="assets/malioboro.jpg" alt="Midnight at Malioboro">
+                <div class="info">
+                    <p>Skysphire</p>
+                    <p>Malioboro</p>
+                    <p>Rp 99.000</p>
+                </div>
+            </div>
+            <!-- pop up novel malioboro-->
+            <div class="modal" id="modal5">
+                <div class="modal-content">
+                    <span class="close" data-close="modal5">&times;</span>
+                    <h2>Midnight At Malioboro</h2>
+                    <p>Tengah malam bagi kebanyakan orang adalah waktu terbaik untuk beristirahat dan tidur lelap. Namun tidak demikian bagi sekelompok anak muda yang merindukan perjalanan dan pengalaman berharga yang akan selalu terukir di ingatan. Dalam novel ini, mereka berpetualang mengarungi malam di Malioboro, menjelajahi keindahan kota Yogyakarta dengan segala keceriaan dan keunikannya. Akankah mereka mendapatkan pengalaman yang tak terlupakan di tengah keramaian kota yang tak pernah mati ini?
+                    </p>
+                </div>
+            </div>
+            <!-- card novel mashle -->
+            <div class="card active" id="card6">
+                <img src="assets/mashle02.jpg" alt="Mashle">
+                <div class="info">
+                    <p>Hajime Komoto</p>
+                    <p>Mashle-02</p>
+                    <p>Rp 36.000</p>
+                </div>
+            </div>
+            <!-- pop up novel mashle -->
+            <div class="modal" id="modal6">
+                <div class="modal-content">
+                    <span class="close" data-close="modal6">&times;</span>
+                    <h2>Mashle</h2>
+                    <p>Mashle yang dirilis dalam bahasa Inggris dengan judul Mashle: Magic and Muscles, adalah sebuah seri manga yang menceritakan kisah Mash Burnedead, seorang pemuda yang lahir tanpa kekuatan sihir di dunia yang diisi oleh penyihir. Ia bertekad untuk menjadi 'Pahlawan' dengan cara melawan para penyihir dan menunjukkan bahwa kekuatan otot bisa mengalahkan kekuatan sihir. Dengan humor yang konyol dan aksi yang menghibur, Mashle menghadirkan petualangan yang seru bagi para pembacanya.
+                    </p>
+                </div>
+            </div>
+            <!-- card matahari -->
+            <div class="card active" id="card7">
+                <img src="assets/matahari.jpg" alt="Matahari">
+                <div class="info">
+                    <p>Tere Liye</p>
+                    <p>Matahari</p>
+                    <p>Rp 101.000</p>
+                </div>
+            </div>
+            <!-- pop up novel matahari -->
+            <div class="modal" id="modal7">
+                <div class="modal-content">
+                    <span class="close" data-close="modal7">&times;</span>
+                    <h2>Matahari</h2>
+                    <p>Namanya Ali, 15 tahun, kelas X. Di kelasnya, Ali adalah salah satu siswa yang pendiam dan tertutup. Namun ia menyimpan banyak rahasia, termasuk perasaannya kepada Rania, teman sekelasnya yang selalu ceria. Dalam perjalanan hidupnya, Ali belajar tentang cinta, persahabatan, dan bagaimana menghadapi tantangan yang datang. Tere Liye berhasil merangkai kisah yang menyentuh hati dan penuh makna dalam novel ini, menghadirkan pengalaman yang akan membekas di hati para pembacanya.
+                    </p>
+                </div>
+            </div>
+            <!-- card novel melangkah -->
+            <div class="card active" id="card8">
+                <img src="assets/melangkah.jpg" alt="Melangkah">
+                <div class="info">
+                    <p>Js Khairen</p>
+                    <p>Melangkah</p>
+                    <p>Rp 95.000</p>
+                </div>
+            </div>
+            <!-- pop up novel melangkah -->
+            <div class="modal" id="modal8">
+                <div class="modal-content">
+                    <span class="close" data-close="modal8">&times;</span>
+                    <h2>Melangkah</h2>
+                    <p>Novel Melangkah
+                        Melangkah adalah novel yang menceritakan tentang perjalanan hidup seorang gadis bernama Aisyah, yang harus menghadapi berbagai rintangan dan tantangan dalam hidupnya. Dengan semangat dan tekad yang kuat, Aisyah berusaha untuk melangkah maju dan menghadapi segala hal yang datang kepadanya. Dalam novel ini, pembaca akan diajak merenungkan makna kehidupan dan belajar dari pengalaman yang dialami oleh Aisyah.
+                    </p>
+                </div>
+            </div>
+            <button class="prev"><</button>
+            <button class="next">></button>
+        </div>
+    </div>
+    <div class="comment-container">
+        <h1>Saran Novel</h1>
+        <br>
+        <form class="comment-form" action="tambah.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="novel_name">Nama Novel</label>
+                <input type="text" class="form-comment" name="novel_name" id="novel_name" placeholder="Masukkan Nama Novel">
+            </div>
+            <div class="form-group">
+                <label for="reason">Alasan</label>
+                <textarea class="form-comment" name="reason" id="reason" placeholder="Masukkan Alasan Kenapa Anda Ingin Novel Ini" style="height: 100px;"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="image">Gambar Novel</label>
+                <input type="file" class="form-comment" name="image" id="image">
+            </div>  
+            <button type="submit" name="submit" id="submit">Submit</button>
+        </form>
+    </div>
+    <!-- Main Content End -->
+    <!-- Include JavaScript -->
+    <script src="script/script.js"></script>
+</body>
+</html>
